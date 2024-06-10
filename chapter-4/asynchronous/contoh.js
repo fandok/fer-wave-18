80ff542917d51ec53b161bd4d61d6070dc2e8e61
fandok <fandika.okdiba@gmail.com>
2024-06-10 21:31:24 +0700

    async functions

---
+++ chapter-4/asynchronous/contoh.js
@@ -0,0 +1,12 @@
+// synchronous
+// console.log("Hello Binarian!");
+// console.log("Javascript");
+// console.log("Developer");
+
+// async
+console.log("Hello Binarian!");
+setTimeout(() => {
+  console.log("Javascript");
+}, 100);
+console.log("Developer");
+console.log("Saya Berhasil");
