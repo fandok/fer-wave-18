7656ba5f44fca25e3717b514908ab8f2628f96aa
fandok <fandika.okdiba@gmail.com>
2024-06-14 21:42:27 +0700

    add import rules

--- chapter-5/my-app/index.js
+++ chapter-5/my-app/index.js
@@ -1,14 +1,14 @@
-// const os = require("os");
-// const luasSegitiga = require("./segitiga");
 import os from "os";
 import luas from "./segitiga.js";
 import chalk from "chalk";
 import currencyFormatter from "currency-formatter";
 
+const { luasSegitiga, luasKotak } = luas;
+
 console.log("Free memory", os.freemem());
 
-console.log(luas.luasSegitiga(3, 4));
-console.log(luas.luasKotak(4, 5));
+console.log(luasSegitiga(3, 4));
+console.log(luasKotak(4, 5));
 
 console.log(chalk.red("Hello world!"));
 
